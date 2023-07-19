99f448a72f7152ebe6211b9aea876c1ac5691e02
Professor Ragna <jpbullalayao@gmail.com>
2023-07-19 16:37:50 -0700

    Setup for PyPI (#3)
    * Add setup for PyPI
    
    * Use limitless again
    
    * Add installation instructions

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+	name="limitless-python",
+	version="1.0.0",
+	author="Professor Ragna",
+	author_email="professor.ragna@gmail.com",
+	long_description="Python wrapper for the Limitless API",
+	url="https://github.com/jpbullalayao/limitless-python",
+	packages=setuptools.find_packages(),
+	license="MIT",
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+)
