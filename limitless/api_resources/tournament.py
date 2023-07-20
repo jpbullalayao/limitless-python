93dfdc77db1b7c2a057d34bdff49f449dda2db24
Professor Ragna <jpbullalayao@gmail.com>
2023-07-19 18:27:40 -0700

    Add static_request helper to APIResource (#5)

--- limitless/api_resources/tournament.py
+++ limitless/api_resources/tournament.py
@@ -1,9 +1,7 @@
 from limitless import api_endpoints
 from limitless import http_methods
-from limitless.api_requestor import APIRequestor
 
 from limitless.api_resources.abstract.listable_api_resource import ListableAPIResource
-from limitless.util import convert_to_limitless_object
 class Tournament(ListableAPIResource):
     RESOURCE_NAME = "tournament"
     FIELD_PK = "id"
@@ -16,33 +14,24 @@ class Tournament(ListableAPIResource):
 
     @classmethod
     def get_details(cls, api_token=None, **params):
-        requestor = APIRequestor(api_token)
-
         pk = params.get(Tournament.FIELD_PK)
         url = Tournament.base_url(pk) + api_endpoints.TOURNAMENT_DETAILS
-        response = requestor.request(http_methods.HTTP_METHOD_GET, url)
-        limitless_object = convert_to_limitless_object(response, cls)
+        response = cls.static_request(http_methods.HTTP_METHOD_GET, url, api_token)
 
-        return limitless_object
+        return response
 
     @classmethod
     def get_standings(cls, api_token=None, **params):
-        requestor = APIRequestor(api_token)
-
         pk = params.get(Tournament.FIELD_PK)
         url = Tournament.base_url(pk) + api_endpoints.TOURNAMENT_STANDINGS
-        response = requestor.request(http_methods.HTTP_METHOD_GET, url)
-        limitless_object = convert_to_limitless_object(response, cls)
+        response = cls.static_request(http_methods.HTTP_METHOD_GET, url, api_token)
 
-        return limitless_object
+        return response
 
     @classmethod
     def get_pairings(cls, api_token=None, **params):
-        requestor = APIRequestor(api_token)
-
         pk = params.get(Tournament.FIELD_PK)
         url = Tournament.base_url(pk) + api_endpoints.TOURNAMENT_PAIRINGS
-        response = requestor.request(http_methods.HTTP_METHOD_GET, url)
-        limitless_object = convert_to_limitless_object(response, cls)
+        response = cls.static_request(http_methods.HTTP_METHOD_GET, url, api_token)
 
-        return limitless_object
+        return response
